3e643d36c8f959f3052434accb76e7f636cbe373
craigwillard <cwillard@tammantech.com>
2013-08-20 14:08:36 -0400

    initial rails project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wdim::Application.config.secret_token = 'e27b94dfa4b889f690404c27549c865f6fd761676441466605fe3887d28ca719e28221faa7c2db6921637657f63507f516b2b589cdd38bc4d01b089b48fddea1'
