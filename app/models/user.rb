62687c8c90402cb66e2e5401146f28d120ef3681
craigwillard <cwillard@tammantech.com>
2013-09-11 16:17:44 -0400

    updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,42 +1,29 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  belongs_to :messages
-  belongs_to :avatars
+  has_many          :avatars
+  # has_many          :messages
 
-  validates       :username,
-                    presence: true,
-                    length: { minimum: 2, maximum: 45 }
+  validates         :username,
+                      presence: true,
+                      length: { minimum: 2, maximum: 45 }
 
-  validates       :password,
-                    presence: true,
-                    length: { minimum: 5, maximum: 25 },
-                    on: :create
+  validates         :password,
+                      presence: true,
+                      length: { minimum: 5, maximum: 25 },
+                      on: :create
 
-  validates       :first_name,
-                    presence: true,
-                    length: { minimum: 2, maximum: 45 }
+  validates         :first_name,
+                      presence: true,
+                      length: { minimum: 2, maximum: 45 }
 
-  validates       :last_name,
-                    presence: true,
-                    length: { minimum: 2, maximum: 45 }
+  validates         :last_name,
+                      presence: true,
+                      length: { minimum: 2, maximum: 45 }
 
-  validates       :email,
-                    presence: true,
-                    format: {with: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
+  validates         :email,
+                      presence: true,
+                      format: {with: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
 
-  # validates       :subdomain,
-  #                   presence: true,
-  #                   uniqueness: {:case_sensitive => false},
-  #                   length: { minimum: 2, maximum: 45 }
-
-  # validates       :talent_mode,
-  #                   presence: true,
-  #                   inclusion: {in: ["system", "local"]}
-
-  # validates       :status,
-  #                   presence: true,
-  #                   inclusion: { in: STATUSES }
-
-  attr_accessible :active, :aim, :at_work, :email, :first_name, :last_name, :member_since, :old_id, :password_digest, :posts_count, :user_type, :username, :password, :password_confirmation
+  attr_accessible   :active, :aim, :at_work, :email, :first_name, :last_name, :member_since, :old_id, :password_digest, :posts_count, :user_type, :username, :password, :password_confirmation
 end
